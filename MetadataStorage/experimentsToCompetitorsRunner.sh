ca2d9896988d1905892debd5e67706c132e472a1
serginf <snadal@essi.upc.edu>
2019-12-19 09:41:03 +0100

    experiments to competitors

---
+++ MetadataStorage/experimentsToCompetitorsRunner.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+for UPPER_BOUND_FEATURES_IN_G in 5 10 20
+do
+	for N_EDGES_IN_QUERY in 2 4 6 8 10 12
+	do
+		for N_WRAPPERS in 2 4 8 16 32 64 128
+		do
+			for N_EDGES_COVERED_BY_WRAPPERS in 2 4 6 8 10 12
+			do 		
+				if [ $N_EDGES_COVERED_BY_WRAPPERS -le $N_EDGES_IN_QUERY ]; then
+					for COVERED_FEATURES_QUERY in 1
+					do
+						for COVERED_FEATURES_WRAPPER in 1
+						do
+							timeout 10m java -jar build/libs/ODIN-1.0.jar 50 $UPPER_BOUND_FEATURES_IN_G $N_EDGES_IN_QUERY $N_WRAPPERS $N_EDGES_COVERED_BY_WRAPPERS $COVERED_FEATURES_QUERY $COVERED_FEATURES_WRAPPER
+							sysctl -w vm.drop_caches=3 > /dev/null #free memory
+							sync && echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null #free memory
+							timeout 10m java -jar build/libs/ODIN-1.0.jar 50 $UPPER_BOUND_FEATURES_IN_G $N_EDGES_IN_QUERY $N_WRAPPERS $N_EDGES_COVERED_BY_WRAPPERS $COVERED_FEATURES_QUERY $COVERED_FEATURES_WRAPPER
+							sysctl -w vm.drop_caches=3 > /dev/null #free memory
+							sync && echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null #free memory
+							timeout 10m java -jar build/libs/ODIN-1.0.jar 50 $UPPER_BOUND_FEATURES_IN_G $N_EDGES_IN_QUERY $N_WRAPPERS $N_EDGES_COVERED_BY_WRAPPERS $COVERED_FEATURES_QUERY $COVERED_FEATURES_WRAPPER
+							sysctl -w vm.drop_caches=3 > /dev/null #free memory
+							sync && echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null #free memory
+						done
+					done
+				fi
+			done
+		done
+	done
+done
