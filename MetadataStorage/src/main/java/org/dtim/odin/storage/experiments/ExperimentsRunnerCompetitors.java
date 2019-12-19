70f0f6eeacd6e3aff7dc3583670ca6cfe1a1e2ea
serginf <snadal@essi.upc.edu>
2019-12-19 15:28:22 +0100

    remove minicon from experiments execution

--- MetadataStorage/src/main/java/org/dtim/odin/storage/experiments/ExperimentsRunnerCompetitors.java
+++ MetadataStorage/src/main/java/org/dtim/odin/storage/experiments/ExperimentsRunnerCompetitors.java
@@ -87,13 +87,14 @@ public class ExperimentsRunnerCompetitors {
         //edges in query; number of covering wrappers;
         System.out.println("CoreCover;"+UPPER_BOUND_FEATURES_IN_G+";"+N_EDGES_IN_QUERY+";"+N_WRAPPERS+";"+N_EDGES_COVERED_BY_WRAPPERS+";"+COVERED_FEATURES_QUERY+
                 ";"+COVERED_FEATURES_WRAPPER+";"+"1"+";"+corecoverRewritings.size()+";"+(b-a));
+        /*
         a = System.currentTimeMillis();
         Set<String> miniconRewritings = DatalogExperimentsRunner.runMiniCon(DatalogConverter.minimizeDatalog(datalogQueries));
         b = System.currentTimeMillis();
         //edges in query; number of covering wrappers;
         System.out.println("MiniCon;"+UPPER_BOUND_FEATURES_IN_G+";"+N_EDGES_IN_QUERY+";"+N_WRAPPERS+";"+N_EDGES_COVERED_BY_WRAPPERS+";"+COVERED_FEATURES_QUERY+
                 ";"+COVERED_FEATURES_WRAPPER+";"+"1"+";"+miniconRewritings.size()+";"+(b-a));
-
+        */
 
     }
 }
