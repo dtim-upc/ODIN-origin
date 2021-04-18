ae356478cf33b4c76158b0133c6da0cfe7fb7cd7
Sergi Nadal <snadal@essi.upc.edu>
2021-04-18 19:20:05 +0200

    update to measure medians

--- MetadataStorage/src/main/java/org/dtim/odin/storage/tests/resultsParser/measureMedians.java
+++ MetadataStorage/src/main/java/org/dtim/odin/storage/tests/resultsParser/measureMedians.java
@@ -5,22 +5,34 @@ import com.google.common.collect.Maps;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class measureMedians {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("|F|;|E_Q|;|W|;|E_W|;Frac_Q;Frac_W;SIZE_OF_INTERMEDIATE_RESULTS;|UCQ|;R (ms)");
+        System.out.println("|F|;|E_Q|;|W|;|E_W|;Frac_Q;Frac_W;SIZE_OF_INTERMEDIATE_RESULTS;|UCQ|;R (ms);Timeout (ms);MiniCon (ms);Graal (ms)");
+        //System.out.println("|F|;|E_Q|;|W|;|E_W|;Frac_Q;Frac_W;R (ms);Timeout (ms);MiniCon (ms);Graal (ms)");
+        //System.out.println("|F|;|E_Q|;|W|;|E_W|;Frac_Q;Frac_W;R (ms);Timeout (ms);Graal (ms)");
 
-        String path = "/home/snadal/Desktop/20191001.txt";
+        String path = "/home/snadal/Desktop/mar2021_experiments/completequeries_raw_2.csv";
+
+        List<String> rawLines = Lists.newArrayList();
+        Files.readAllLines(new File(path).toPath()).forEach(rawLines::add);
+
+        List<String> lines = Lists.newArrayList();
+        for (int i = 0 ; i < rawLines.size(); i+=2) {
+            lines.add(rawLines.get(i) + ";" + rawLines.get(i+1));
+        }
 
         Map<String, List<String>> allData = Maps.newHashMap();
-        Files.readAllLines(new File(path).toPath()).forEach(line -> {
+        lines.forEach(line -> {
             String[] split = line.split(";");
 
             String dimensions = split[0]+";"+split[1]+";"+split[2]+";"+split[3]+";"+split[4]+";"+split[5];
-            String values = split[6]+";"+split[7]+";"+split[8];
+            //ignore 6 and 7
+            String values = split[8]+";"+split[9]+";"+split[10];//+";"+split[11];
             allData.putIfAbsent(dimensions, Lists.newArrayList());
 
             List<String> oldList = allData.get(dimensions);
@@ -30,6 +42,8 @@ public class measureMedians {
         });
 
         allData.forEach((dimension,values) -> {
+            // AVG
+            /*
             int X = 0, Y = 0, Z = 0;
             int N = 0;
             for(String v : values) {
@@ -40,6 +54,20 @@ public class measureMedians {
             };
             System.out.print(dimension+";");
             System.out.println((double)(X)/(double)(N)+";"+(double)(Y)/(double)(N)+";"+(double)(Z)/(double)(N));
+            */
+
+            //MEDIAN
+            System.out.print(dimension);
+            for (int i = 0; i < values.get(0).split(";").length; ++i) {
+                List<Double> all = Lists.newArrayList();
+                for (int j = 0; j < values.size(); ++j) {
+                    all.add(Double.parseDouble(values.get(j).split(";")[i]));
+                }
+                Collections.sort(all);
+                System.out.print(";"+all.get(all.size()/2).intValue());
+            }
+            System.out.println(";0");
+
             /*
             values.sort((s, t1) -> {
                 int a = Integer.parseInt(s.split(";")[2]);
